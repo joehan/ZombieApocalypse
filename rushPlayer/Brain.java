eb08207c3f9ef772670b4799a46984e325818a96
CSparano-Huiban <csparano@mit.edu>
2016-01-09 13:12:04 -0500

    copied in brain

--- rushPlayer/Brain.java
+++ rushPlayer/Brain.java
@@ -1,6 +1,9 @@
 package rushPlayer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import battlecode.common.*;
 
 /*
@@ -11,5 +14,36 @@ import battlecode.common.*;
  */
 public class Brain {
 	
-	private ArrayList<MapLocation> denLocations = new ArrayList<MapLocation>();
+	//Rembering den locations!
+	private HashSet<MapLocation> denLocations = new HashSet<MapLocation>();
+	public MapLocation[] getDenLocations(){
+		return denLocations.toArray(new MapLocation[denLocations.size()]);
+	}
+	public void addDenLocation(MapLocation loc) {
+		denLocations.add(loc);
+	}
+	public void removeDenLocation(MapLocation loc){
+		denLocations.remove(loc);
+	}
+	public Boolean isDenNew(MapLocation den) {
+		return !(denLocations.contains(den));
+	}
+	
+	//Remembering what you've built
+	public HashMap<RobotType, Integer> buildHistory; 
+	
+	public void initBuildHistory(){
+		buildHistory = new HashMap<RobotType, Integer>();
+		buildHistory.put(RobotType.GUARD, 0);
+		buildHistory.put(RobotType.SCOUT, 0);
+		buildHistory.put(RobotType.SOLDIER, 0);
+		buildHistory.put(RobotType.TURRET, 0);
+		buildHistory.put(RobotType.VIPER, 0);
+	}
+	
+	public void iterateUnitInBuildHistory(RobotType type){
+		buildHistory.put(type, buildHistory.get(type) +1);
+	}
+	
+	
 }
