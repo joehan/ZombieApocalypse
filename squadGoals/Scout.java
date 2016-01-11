547e3594802da68a4422f7827e175b0600a529c5
CSparano-Huiban <csparano@mit.edu>
2016-01-10 22:12:52 -0500

    random move scouts

--- squadGoals/Scout.java
+++ squadGoals/Scout.java
@@ -11,20 +11,20 @@ public class Scout {
 		Direction randomDir = Entity.directions[rand.nextInt(8)];
 		
 		while (true) {
-			if (rc.isCoreReady()) {
-				MapLocation nearestDen = Entity.searchForDen(rc);
+//			if (rc.isCoreReady()) {
+//				MapLocation nearestDen = Entity.searchForDen(rc, brain);
+//				
+//				//If we see a new den
+//				if ( (!(nearestDen.equals(rc.getLocation()))) && brain.isDenNew(nearestDen) ){
+//					Entity.signalMessageLocation(rc, nearestDen);
+//					rc.setIndicatorString(1, "Found den at " + nearestDen.x + ", " + nearestDen.y);
+//				} else {
+				Entity.moveInDirection(rc, randomDir);
+				randomDir = Entity.directions[rand.nextInt(8)];
+				rc.setIndicatorString(1, "Moving Random "+ randomDir.toString());
+//				} 
 				
-				//If we see a new den
-				if ( (!(nearestDen.equals(rc.getLocation()))) && brain.isDenNew(nearestDen) ){
-					Entity.signalMessageLocation(rc, nearestDen);
-					rc.setIndicatorString(1, "Found den at " + nearestDen.x + ", " + nearestDen.y);
-				} else {
-					Entity.moveInDirection(rc, randomDir);
-					randomDir = Entity.directions[rand.nextInt(8)];
-					rc.setIndicatorString(1, "Moving Random "+ randomDir.toString());
-				} 
-				
-			}
+//			}
 			Clock.yield();
 		}
 	}
