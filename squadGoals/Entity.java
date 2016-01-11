0fab1e31cf6e0cd2e617ba68d51fae5c60a9c1ef
Brian Copeland <bcope@mit.edu>
2016-01-11 18:17:20 -0500

    improve stuff

--- squadGoals/Entity.java
+++ squadGoals/Entity.java
@@ -255,7 +255,6 @@ public class Entity {
 		return null;
 	}
 	
-	
 	public static Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
             Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
 	
