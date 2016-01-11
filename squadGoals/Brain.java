36e005bf40eea2b8270c5b93f535118b2ec8492f
CSparano-Huiban <csparano@mit.edu>
2016-01-10 22:05:27 -0500

    merged in

--- squadGoals/Brain.java
+++ squadGoals/Brain.java
@@ -49,7 +49,7 @@ public class Brain {
 	}
 	
 	private RobotType[] startBuildArray = {RobotType.SCOUT, RobotType.SOLDIER};
-	private RobotType[] iterateBuildArray = {RobotType.GUARD, RobotType.SOLDIER, RobotType.SOLDIER};
+	private RobotType[] iterateBuildArray = {RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER};
 	private int buildCount = 0;
 	private Boolean initialIteration = true;
 	
